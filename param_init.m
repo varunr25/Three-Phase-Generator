810a7305d80cdfc1165c381c1efc76af705df05c
varunr25 <varunjunk25@gmail.com>
2024-04-23 15:56:59 -0400

    complete

--- param_init.m
+++ param_init.m
@@ -5,16 +5,22 @@
 %% Initialization
 Eamp = 392;     Wac = 377;     Lac = 1e-3;     k = 1;     phi = 2 * pi / 3;
 
-dt = 1e-7;                  tend = 10 * 2 * pi / Wac;   t = linspace(0, tend, tend/dt);
+dt = 1e-7;      Tac = 2 * pi / Wac;     tend = 10 * Tac;      t = 0:dt:(2 * (2 * pi / Wac)); 
 
-ias = zeros(size(t));       ibs = zeros(size(t));       ics = zeros(size(t));
+ias = zeros(1, length(t));      ibs = zeros(1, length(t));      ics = zeros(1, length(t));
 
-Eas = zeros(size(t));       Ebs = zeros(size(t));       Ecs = zeros(size(t));
+Eas = zeros(1, length(t));      Ebs = zeros(1, length(t));      Ecs = zeros(1, length(t));
 
-Va = zeros(size(t));        Vb = zeros(size(t));        Vc = zeros(size(t));
+Va = zeros(1, length(t));       Vb = zeros(1, length(t));       Vc = zeros(1, length(t));
 
-Vag = zeros(size(t));       Vbg = zeros(size(t));       Vcg = zeros(size(t));
+Vag = zeros(1, length(t));      Vbg = zeros(1, length(t));      Vcg = zeros(1, length(t));
 
-iD1 = zeros(1, size(t));    iD3 = zeros(1, size(t));    iD5 = zeros(1, size(t));
+iD1 = zeros(1, length(t));      iD3 = zeros(1, length(t));      iD5 = zeros(1, length(t));
 
-Vdc = zeros(1, size(t));    Vdc_p = zeros(1, size(t));  idc = zeros(1, size(t));    
+Vdc = zeros(1, length(t));      Vdc_p = zeros(1, length(t));    idc = zeros(1, length(t));
+
+idc_M1 = zeros(1, length(t));   idc_M2 = zeros(1, length(t));   idc_M3 = zeros(1, length(t));
+
+int_term = zeros(1, length(t)); theta = zeros(1, length(t));       
+
+gamma = 45;     alpha = 10;     tau = 1e-5;     esp = 0.01;
